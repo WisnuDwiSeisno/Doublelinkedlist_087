44c56ad34df931b0b1c542bedef5cd635a5be2b8
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-26 16:00:34 +0700

    membuat method untuk memasukan newnode di awal

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -23,5 +23,32 @@ public:
 	void revtraverse();
 	void hapus();
 	void searchData();
-
 };
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name od the student: ";
+	cin >> nm;
+
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; //step 2
+	newNode->name = nm; //step 2
+
+	/*insert a node in the begining of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) {//check if data null
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START; //step 3
+		if (START != NULL)
+			START->prev = newNode; //step 4
+		newNode->prev = NULL; //step 5
+		START = newNode; //step 6
+		return;
+	}
+
+}
