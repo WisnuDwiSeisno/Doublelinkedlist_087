6dd3555ba39f8f1545a1d47dae0b5ad67d17ce63
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-26 16:18:29 +0700

    menambahkan boolean untuk method search dan delete node

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -71,3 +71,28 @@ void DoubleLinkedList::addNode() {
 		current->next->prev = newNode; //step 6
 	current->next = newNode; //step 7
 }
+
+bool DoubleLinkedList::search(int rollNo, Node** previous, Node** current) {
+	*previous = NULL; //step 1. a
+	*current = START; // 1. b
+	while (*current != NULL && (*current)->noMhs != rollNo) { //1. c
+		*previous = *current; //1. d
+		*current = (*current)->next; // 1. e
+	}
+	return (*current != NULL);
+}
+
+bool DoubleLinkedList::deleteNode(int rollNo) {
+	Node* previous, * current;
+	previous = current = NULL;
+	if (search(rollNo, &previous, &current) == false)
+		return false;
+	if (current->next != NULL)
+		current->next->prev = previous; //step 2
+	if (previous != NULL)
+		previous->next = current->next; //step 3
+	else
+		START = current->next;
+	delete current; //step 4
+	return true;
+}
