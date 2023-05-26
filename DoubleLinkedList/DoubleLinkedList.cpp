98470a2e5863cd7c56e86f88371f293bdb2c5ef3
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-26 18:37:02 +0700

    memperbaiki kesalah/typo

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -30,7 +30,7 @@ void DoubleLinkedList::addNode() {
 	string nm;
 	cout << "\nEnter the roll number of the student: ";
 	cin >> nim;
-	cout << "\nEnter the name od the student: ";
+	cout << "\nEnter the name of the student: ";
 	cin >> nm;
 
 	Node* newNode = new Node(); //step 1
